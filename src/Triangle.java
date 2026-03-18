b519a6859c884e20e6684145ea2f6ef7730bea2e
Neha <nehaphilip0205@gmail.com>
2026-03-18 10:23:19 +0530

    Completed Triangle program

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,12 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String args[]){
+		int i,j,k=1;
+		for(i=0;i<3;i++)
+		{
+			for(j=0;j<k-1;j++)
+				System.out.print("* ");
+			System.out.println("*");
+			k+=2;
+		}
+	}
 }
